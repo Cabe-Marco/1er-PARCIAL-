9fd2cf83832569b88958a6ca16ef03b7df252069
Cabe-Marco <cabellomarco@hotmail.com>
2023-05-29 01:26:00 -0300

    Subida archivos examen

--- 1er_Parcial.py
+++ 1er_Parcial.py
@@ -9,31 +9,30 @@ def leer_archivo(archivo_json: str) -> list:
  
 def mostrar_Nombre_y_Posicion_de_Jugadores(lista_jugadores:list):       
     longitud_lista_jugadores = len(lista_jugadores)                     
-    print("\n")                                                           
+print("\n")                                                           
     for i in range(longitud_lista_jugadores):
         jugador_nombre = lista_jugadores[i]["nombre"]
         jugador_posicion = lista_jugadores[i]["posicion"]
 
         mensaje = "{0} - {1}".format(jugador_nombre,jugador_posicion)
-        print(mensaje)
+    print(mensaje)
         
                                                         ### PROGRAMA ###
 #mostrar_Nombre_y_Posicion_de_Jugadores(lista_jugadores) #PUNTO 1cx
 
 def mostrar_jugador_y_su_indice(lista_jugadores:list):
     longitud_lista_jugadores = len(lista_jugadores)                       # Nombre Jugador - Posición. Ejemplo:     
-    print("\n")                                                           # Michael Jordan - Escolta  
+print("\n")                                                           # Michael Jordan - Escolta  
     for i in range(longitud_lista_jugadores):
         jugador_nombre = lista_jugadores[i]["nombre"]
 
         mensaje = "{0} - {1}".format(jugador_nombre,i)
-        print(mensaje)
+    print(mensaje)
 
 def estadisticas_de_jugador(indice_jugador:int,lista_jugadores:list):
       
     i= int(indice_jugador)
     jugador = lista_jugadores[i]["estadisticas"]
-    #print(jugador)
     return(jugador)
     
 
@@ -51,13 +50,18 @@ def generar_csv(nombre_archivo:str, lista, lista_jugadores,indice_jugador:int):
         texto_nombre_y_posicion += texto    
         archivo.write(texto_nombre_y_posicion)
 
+def imprimir_estadisticas_jugador_seleccionado(lista_jugadores):
+    mostrar_jugador_y_su_indice(lista_jugadores)  #MUESTRO EL NOMBRE Y SU INDICE DE JUGADOR
+    indice_jugador = input("Ingrese el indice del jugador que desee ") #se ingresa el indice del jugadora que queremos saber sus estadisticas
+    estadisticas_del_jugador_seleccionado = estadisticas_de_jugador(indice_jugador,lista_jugadores)
+print(estadisticas_del_jugador_seleccionado)
+
 lista_jugadores = leer_archivo(r"C:\Users\Marquillos\Desktop\PARCIAL 1ER PROGRAMACION\1er-PARCIAL-\Dream_Team.json") #creo una lista con los datos de todo los jugadores sin el nombre del team
 
-#mostrar_jugador_y_su_indice(lista_jugadores)  #MUESTRO EL NOMBRE Y SU INDICE DE JUGADOR
 
-#indice_jugador = input("Ingrese el indice del jugador que desee ") #se ingresa el indice del jugadora que queremos saber sus estadisticas
 
-#estadisticas_del_jugador_seleccionado = estadisticas_de_jugador(indice_jugador,lista_jugadores)
+
+
 #estadisticas_de_jugador(indice_jugador,lista_jugadores)
 
 ''' 
@@ -67,24 +71,23 @@ promedio de puntos por partido, rebotes totales, promedio de rebotes por partido
 robos totales, bloqueos totales, porcentaje de tiros de campo, porcentaje de tiros libres y porcentaje de tiros triples.
 '''
 
-#print(lista_jugadores[0]["nombre"])
-generar_csv(r"C:\Users\Marquillos\Desktop\PROGRAMACION\ARCHIVOS\EXAMEN\Dr_Team.csv",estadisticas_del_jugador_seleccionado,lista_jugadores,indice_jugador)
+#generar_csv(r"C:\Users\Marquillos\Desktop\PROGRAMACION\ARCHIVOS\EXAMEN\Dr_Team.csv",estadisticas_del_jugador_seleccionado,lista_jugadores,indice_jugador)
 
-while(True):
-    print("\n")
-    print("MENU OPCIONES:")
-    print("Punto 1")
-    print("Punto 2")
-    print("Punto 3")
+#while(True):
+print("\n")
+print("MENU OPCIONES:")
+print("Punto 1")
+print("Punto 2")
+print("Punto 3")
+print("0 para Finalizar")
 
-    opcion = input("\nElige un opcion:......")
-
-    match (opcion):
-        case "1":
-            mostrar_Nombre_y_Posicion_de_Jugadores(lista_jugadores)
-        case "2":
-            busca_en_dict_segun_clave_y_genero(lista_personajes,"F","genero")
-        case "3":
-            busca_en_dict_segun_clave_y_genero(lista_personajes,"M","altura")
-        case _:
-            break
+opcion = input("\nElige un opcion:......")
+match (opcion):
+    case "1":
+        mostrar_Nombre_y_Posicion_de_Jugadores(lista_jugadores)
+    case "2":
+        imprimir_estadisticas_jugador_seleccionado(lista_jugadores)
+    case "3":
+        pass
+    case "d":
+        break
