d7b69166a837242709fa2814e698d37f9fa80808
Cabe-Marco <cabellomarco@hotmail.com>
2023-05-29 02:00:49 -0300

    Subida archivos examen

--- 1er_Parcial.py
+++ 1er_Parcial.py
@@ -7,22 +7,19 @@ def leer_archivo(archivo_json: str) -> list:
         lista_jugadores = diccionario["jugadores"]
     return lista_jugadores
  
-def mostrar_Nombre_y_Posicion_de_Jugadores(lista_jugadores:list):       
-    longitud_lista_jugadores = len(lista_jugadores)                     
-    print("\n")                                                           
+def mostrar_Nombre_y_Posicion_de_Jugadores(lista_jugadores:list):     #PUNTO 1cx  
+    longitud_lista_jugadores = len(lista_jugadores)                   # Nombre Jugador - Posición  
+    print("\n")                                                       #Ejemplo: Michael Jordan - Escolta      
     for i in range(longitud_lista_jugadores):
         jugador_nombre = lista_jugadores[i]["nombre"]
         jugador_posicion = lista_jugadores[i]["posicion"]
 
         mensaje = "{0} - {1}".format(jugador_nombre,jugador_posicion)
         print(mensaje)
-        
-                                                        ### PROGRAMA ###
-#mostrar_Nombre_y_Posicion_de_Jugadores(lista_jugadores) #PUNTO 1cx
-
-def mostrar_jugador_y_su_indice(lista_jugadores:list):
-    longitud_lista_jugadores = len(lista_jugadores)                       # Nombre Jugador - Posición. Ejemplo:     
-    print("\n")                                                           # Michael Jordan - Escolta  
+                                                       
+def mostrar_jugador_y_su_indice(lista_jugadores:list):                 
+    longitud_lista_jugadores = len(lista_jugadores)                        
+    print("\n")                                                        
     for i in range(longitud_lista_jugadores):
         jugador_nombre = lista_jugadores[i]["nombre"]
 
@@ -35,7 +32,6 @@ def estadisticas_de_jugador(indice_jugador:int,lista_jugadores:list):
     jugador = lista_jugadores[i]["estadisticas"]
     return(jugador)
     
-
 def generar_csv(nombre_archivo:str, lista, lista_jugadores,indice_jugador:int):
     with open(nombre_archivo, "w") as archivo:
         indice_jugador= int(indice_jugador)
@@ -50,29 +46,21 @@ def generar_csv(nombre_archivo:str, lista, lista_jugadores,indice_jugador:int):
         texto_nombre_y_posicion += texto    
         archivo.write(texto_nombre_y_posicion)
 
-def imprimir_estadisticas_jugador_seleccionado():
+def imprimir_estadisticas_jugador_seleccionado(lista_jugadores):
     mostrar_jugador_y_su_indice(lista_jugadores)  #MUESTRO EL NOMBRE Y SU INDICE DE JUGADOR
-    indice_jugador = input("Ingrese el indice del jugador que desee ") #se ingresa el indice del jugadora que queremos saber sus estadisticas
+    indice_jugador = input("Ingrese el indice del jugador que desea imprimir ") #se ingresa el indice del jugadora que queremos saber sus estadisticas
     estadisticas_del_jugador_seleccionado = estadisticas_de_jugador(indice_jugador,lista_jugadores)
-    print(estadisticas_del_jugador_seleccionado)
+    print(estadisticas_del_jugador_seleccionado)    
+
+def imprimir_estadisticas_en_el_CSV(lista_jugadores):
+    mostrar_jugador_y_su_indice(lista_jugadores) 
+    indice_jugador = input("Ingrese el indice del jugador que desea imprimir ")
+    estadisticas_del_jugador_seleccionado = estadisticas_de_jugador(indice_jugador,lista_jugadores)
+    generar_csv(r"C:\Users\Marquillos\Desktop\PROGRAMACION\ARCHIVOS\EXAMEN\Dr_Team.csv",estadisticas_del_jugador_seleccionado,lista_jugadores,indice_jugador)
+
+                                                                       ### PROGRAMA ###
 
 lista_jugadores = leer_archivo(r"C:\Users\Marquillos\Desktop\PARCIAL 1ER PROGRAMACION\1er-PARCIAL-\Dream_Team.json") #creo una lista con los datos de todo los jugadores sin el nombre del team
-
-
-
-
-
-#estadisticas_de_jugador(indice_jugador,lista_jugadores)
-
-''' 
-#3  Después de mostrar las estadísticas de un jugador seleccionado por el usuario, permite al usuario guardar las estadísticas de ese jugador 
-en un archivo CSV. El archivo CSV debe contener los siguientes campos: nombre, posición, temporadas, puntos totales, 
-promedio de puntos por partido, rebotes totales, promedio de rebotes por partido, asistencias totales, promedio de asistencias por partido, 
-robos totales, bloqueos totales, porcentaje de tiros de campo, porcentaje de tiros libres y porcentaje de tiros triples.
-'''
-
-#print(lista_jugadores[0]["nombre"])
-#generar_csv(r"C:\Users\Marquillos\Desktop\PROGRAMACION\ARCHIVOS\EXAMEN\Dr_Team.csv",estadisticas_del_jugador_seleccionado,lista_jugadores,indice_jugador)
 
 #while(True):
 print("\n")
@@ -90,4 +78,6 @@ match (opcion):
     case "2":
         imprimir_estadisticas_jugador_seleccionado(lista_jugadores)
     case "3":
-        pass
+        imprimir_estadisticas_en_el_CSV(lista_jugadores)
+    case "0":
+        break
